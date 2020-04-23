a89de88931e56acb9f6613c5f83849ceab96c95c
Tiago Pires <tiago.pires@me.com>
2020-04-23 18:35:23 +1000

    Add items to a list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,51 +8,61 @@ var app = {
 
 var addOption = function addOption(e) {
     e.preventDefault();
-    console.log('Add Option funtion');
+    var option = e.target.elements.addBtn.value;
+
+    // add options to array 
+    if (option) app.options.push(option);
+
+    // clear the input 
+    e.target.elements.addBtn.value = '';
+
+    render();
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Your Optios' : 'No Options'
-    ),
-    React.createElement(
-        'ol',
+function render() {
+
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'Item one'
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
         ),
         React.createElement(
-            'li',
+            'p',
             null,
-            'Item two'
-        )
-    ),
-    React.createElement(
-        'form',
-        { onSubmit: addOption },
-        React.createElement('input', { type: 'text', name: 'option' }),
+            app.options.length > 0 ? 'Your Options: ' + app.options.length : 'No Options'
+        ),
         React.createElement(
-            'button',
+            'ol',
             null,
-            'Add Option'
+            app.options.map(function (option, index) {
+                return React.createElement(
+                    'li',
+                    { key: index },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: addOption },
+            React.createElement('input', { type: 'text', name: 'addBtn' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option Still'
+            )
         )
-    )
-);
+    );
 
-ReactDOM.render(template, document.getElementById('app'));
+    ReactDOM.render(template, document.getElementById('app'));
+}
+
+render();
--- src/app.js
+++ src/app.js
@@ -7,25 +7,41 @@ const app = {
 
 const addOption = (e) => {
     e.preventDefault()
-    console.log('Add Option funtion')
+    const option = e.target.elements.addBtn.value;
+    
+    // add options to array 
+    if(option) app.options.push(option)
+
+    // clear the input 
+    e.target.elements.addBtn.value = ''
+
+    render()  
 }
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Your Optios' : 'No Options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-        <form onSubmit={addOption}>
-            <input type="text" name="option" />
-            <button>Add Option</button>
-        </form>
-    </div>
-)
- 
+function render(){
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Your Options: ' + app.options.length  : 'No Options'}</p>
+            <ol>
+            {
+                app.options.map((option, index) => {
+                    return <li key={index}>{option}</li>
+                })
+            }
+            </ol>
+            <form onSubmit={addOption}>
+                <input type="text" name="addBtn" />
+                <button>Add Option Still</button>
+            </form>
+        </div>
+    )
+
+    ReactDOM.render(template, document.getElementById('app'))
+}
+
+render()
 
 
-ReactDOM.render(template, document.getElementById('app'))
