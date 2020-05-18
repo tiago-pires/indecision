12a4b3b50cc08e9da306cf910e261b66aca41b2f
Tiago Pires <tiago.pires@me.com>
2020-05-18 15:49:32 +1000

    Setup webpack and break code into separate files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// entry -> output
+// entry -> loaders -> output
+// entry -> loaders -> plugins -> output
+
+const path = require('path')
+
+module.exports = {
+    entry: './src/app.js', // '.' means here
+    output: {
+        path: path.join(__dirname,'public','scripts'), // absolute path to output, you can use __dirname instead of '.'
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                  presets: [
+                      "@babel/preset-env", 
+                      "@babel/preset-react"
+                    ]
+              }
+            }
+          }
+        ]    
+    }
+}
